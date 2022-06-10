5eb13f9c107d4556925c22494fae996442668017
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-06-10 12:23:25 +0800

    go fighting version 1

--- main.py
+++ main.py
@@ -17,12 +17,86 @@ import os
 import logging
 import random
 from flask import Flask, request
+import numpy
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+campus = ['N', 'E', 'S', 'W']
 moves = ['F', 'T', 'L', 'R']
+
+PRE_MOVE = None
+MY_URL = 'https://cloud-run-hackathon-python-gmlbzotaqa-uc.a.run.app'
+
+class Board:
+    def __init__(self, dims, status):
+        self.width = int(dims[0])
+        self.height = int(dims[1])
+        self.player = dict(
+            x = int(status[MY_URL]['x']),
+            y = int(status[MY_URL]['y']),
+            dir = status[MY_URL]['direction'],
+            hited = status[MY_URL]['wasHit'],
+        )
+        self.status = status
+        self.board = None
+
+    def gen_board(self):
+        self.board = numpy.zeros((self.height, self.width))
+        for purl in self.status:
+            p = self.status[purl]
+            x = int(p['x'])
+            y = int(p['y'])
+            self.board[y][x] = 1
+    
+    def enemy_checker(self,dir):
+        is_enemy = False
+        if dir=='N':
+            for i in range(1,4):
+                xx = self.player['x']
+                yy = self.player['y']-i
+                if (yy)<0: break
+                if self.board[yy][xx]==1: is_enemy=True
+        
+        if dir=='S':
+            for i in range(1,4):
+                xx = self.player['x']
+                yy = self.player['y']+i
+                if (yy)>=self.height: break
+                if self.board[yy][xx]==1: is_enemy=True
+
+        if dir=='W':
+            for i in range(1,4):
+                xx = self.player['x']-i
+                yy = self.player['y']
+                if (xx)<0: break
+                if self.board[yy][xx]==1: is_enemy=True
+
+        if dir=='E':
+            for i in range(1,4):
+                xx = self.player['x']+i
+                yy = self.player['y']
+                if (xx)>=self.width: break
+                if self.board[yy][xx]==1: is_enemy=True
+
+        return is_enemy
+
+    def fighting_mode(self):
+        # select target
+        player_dir = self.player['dir']
+        # forward
+        if self.enemy_checker(player_dir): return 'T'
+        
+        # left side
+        if self.enemy_checker((campus.index(player_dir)+3)%4): return 'L'
+
+        # right side
+        if self.enemy_checker((campus.index(player_dir)+5)%4): return 'R'
+
+        # no enemy
+        return moves[random.randrange(len(moves))]
+
 
 @app.route("/", methods=['GET'])
 def index():
@@ -32,7 +106,16 @@ def index():
 def move():
     request.get_data()
     logger.info(request.json)
-    return moves[random.randrange(len(moves))]
+
+    info = request.json
+    # if isinstance(MY_URL, type(None)):
+    #     MY_URL = info["_links"]["self"]["href"]
+
+    arena = info["arena"]
+    board = Board(arena["dims"], arena["state"])
+    board.gen_board()
+
+    return board.fighting_mode()
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
